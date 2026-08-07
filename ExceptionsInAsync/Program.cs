a79ce00b42959e1df568cba98f7b04e7b309c46c
AIL-PC-307-Windows <aakash.gupta@argusoft.com>
2026-08-07 12:21:19 +0530

    Worked on handling basic Exceptions in AsyncAwait

---
+++ ExceptionsInAsync/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ExceptionsInAsync;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.WriteLine("Before method call");
+
+        try
+        {
+            int result = await GetRiskyNumberAsync();
+            Console.WriteLine($"Got: {result}");
+        }
+        catch (InvalidOperationException ex)
+        {
+            Console.WriteLine($"Caught: {ex.Message}");
+        }
+
+        Console.WriteLine("After method call");
+    }
+
+    static async Task<int> GetRiskyNumberAsync()
+    {
+        Console.WriteLine("Before Delay");
+        await Task.Delay(2000);
+        Console.WriteLine("About to throw");
+        throw new InvalidOperationException("Something went wrong!");
+    }
+}
