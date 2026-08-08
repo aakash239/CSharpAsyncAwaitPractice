ccb4a20a1341f2b331f20174b1be31dae8de97f1
AIL-PC-307-Windows <aakash.gupta@argusoft.com>
2026-08-08 11:14:07 +0530

    Cancellation Mechanism for Asynchronous tasks

---
+++ CancellationAsyncAwait/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace CancellationAsyncAwait;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        CancellationTokenSource token = new CancellationTokenSource();
+        try
+        {
+            Task printNumTask = CountAsync(token.Token);
+            await Task.Delay(3001);
+            token.Cancel();
+            await printNumTask;
+        }
+        catch (OperationCanceledException ex)
+        {
+            Console.WriteLine($"\nOperation was cancelled: {ex.Message}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"\nUnexpected Error: {e.Message}");
+        }
+        finally
+        {
+            Console.WriteLine("Main cleanup done");
+        }
+    
+    }
+
+    static async Task CountAsync(CancellationToken token)
+    {
+        int num = 0;
+
+        try
+        {
+            while(true)
+            {
+                token.ThrowIfCancellationRequested();
+                Console.Write($"{num++} ");
+                await Task.Delay(1000, token);
+            }    
+        }
+        finally
+        {
+        Console.WriteLine("\nCleaning up CountAsync resources...");
+        }
+    }
+}
